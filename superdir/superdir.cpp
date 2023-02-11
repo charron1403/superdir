94973e3cc9c2cfb468556170330f18a49b37694e
Alex Charron <alexwillgo2@gmail.com>
2023-02-10 21:31:41 -0500

    quickfixes

--- superdir/superdir.cpp
+++ superdir/superdir.cpp
@@ -9,16 +9,26 @@
 
 namespace fs = std::filesystem;
 
-
 int main(int argc, const char ** argv)
 {
-	WIN32_FIND_DATA FindFileData;
-	HANDLE hFind;
-
-	IFileInfo** fichiers = Functions::FindFiles(argv[1]);
+	if (argc < 2) {
+		std::cout << "aucun chemin n'est passe en argument\n";
+		return 1;
+	}
 
 	struct stat sb;
 
+	fs::directory_entry entry = fs::directory_entry(argv[1]);
+	fs::path outfilename = entry.path();
+	std::string outfilename_str = outfilename.string();
+	const char* path = outfilename_str.c_str();
+
+	if (stat(path, &sb) == 0 && !(sb.st_mode & S_IFDIR)) {
+		std::cout << "le chemin specifie n'existe pas\n";
+		return 1;
+	}
+
+	IFileInfo** fichiers = Functions::FindFiles(argv[1]);
 	int index_file_array = 0;
 
 	for (const auto& entry : fs::directory_iterator(argv[1])) {
